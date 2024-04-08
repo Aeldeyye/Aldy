efdce974ecff3cd0d25614c77fc6d41a636f7e0e
Aeldeyye <166361701+Aeldeyye@users.noreply.github.com>
2024-04-08 12:21:31 +0700

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/home-page")
 def home():
-    return "Hello & Welcome to My AWS Web Service that is connected in {port}"
+    return "Hello And Welcome to My Web Services"
 
-@app.route("/about-me")
+@app.route("/about-me-page")
 def index():
-    return "About-Me"
+    return "My Name Is Aldy ALfiansyah"
 
 @app.route("/data")
 def data():
-    sample_data = {"Name" : "Harry", "Age" : 20, "Class" : "LC01"}
+    sample_data = {"Name" : "Aldy", "Age" : 20, "Class" : "LC01"}
     return jsonify(sample_data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=7000, debug=True)
 
